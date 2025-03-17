41d856914d20ffcf1d1659cd9490eb1fb29c0c7c
atalienet <atalie@atalie.net>
2025-03-17 21:28:38 +0900

    リザルト画面から緯度経度を削除、やや文章を修正

--- components/locationHeader.tsx
+++ components/locationHeader.tsx
@@ -9,9 +9,7 @@ type LocationHeaderProps = {
 export function LocationHeader({ lat, lng, distance, page, totalPages }: LocationHeaderProps) {
   return (
     <div className="mb-4 text-center">
-      <p>緯度: {lat}, 経度: {lng}</p>
-      <p>半径 {distance}m 以内</p>
-      <p>ページ: {page} / {totalPages}</p>
+      <p>半径 {distance}m 以内のお店が近い順に並んでいます。</p>
     </div>
   );
 }
